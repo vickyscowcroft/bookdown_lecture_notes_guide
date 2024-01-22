e4b0c74b536436768baf43ce13e623900cb86058
Vicky Scowcroft <vs522@bath.ac.uk>
2024-01-22 09:20:06 +0000

    Delete _deploy.sh

--- _deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-git clone -b gh-pages https://github.com/vickyscowcroft/bookdown_lecture_notes_guide.git book-output
-cd book-output
-cp -r ../_book/* ./
-git add --all *
-git commit -m "Update the book" || true
-git push -q origin gh-pages
